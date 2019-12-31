6298fef8bb908a558c432f1a9f1c49fe704d766e
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-01-01 00:06:10 +0100

    #2

--- NumberUtil.Test/PolynomialXTests.cs
+++ NumberUtil.Test/PolynomialXTests.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
-using NumberUtil;
+﻿using Xunit;
+using NumberUtil.PrimeNumbers;
 
 namespace NumberUtil.Test
 {
     public class PolynomialXTests
     {
+        // int
+
         [Fact]
-        public void PolynomialTestingNullArray()
+        public void PolynomialTestingNullArray_int()
         {
             int[] p = null;
 
@@ -19,7 +18,7 @@ namespace NumberUtil.Test
         }
 
         [Fact]
-        public void PolynomialTestingEmptyArray()
+        public void PolynomialTestingEmptyArray_int()
         {
             var p = new int[] { };
 
@@ -32,7 +31,7 @@ namespace NumberUtil.Test
         [InlineData(0, 5)]
         [InlineData(-1, 5)]
         [InlineData(1, 5)]
-        public void PolynomialTestingOneTerm(int x, int c)
+        public void PolynomialTestingOneTerm_int(int x, int c)
         {
             var p = new int[] { c };
 
@@ -50,9 +49,106 @@ namespace NumberUtil.Test
 
         [InlineData(0, 1, new int[] { 5, 2, -4, -3 })]
         [InlineData(660, 5, new int[] { 5, 2, -4, 5 })]
-        public void PolynomialTest(int expected, int x, int[] coefficients)
+        public void PolynomialTest_int(int expected, int x, int[] coefficients)
         {
             Assert.Equal(expected, coefficients.Polynomial(x));
+        }
+
+        [Fact]
+        public void PolynomialTest_EulerProjectProblem27Example1_int()
+        {
+            var coefficients = new int[] { 1, 1, 41 };
+            for (int i = 0; i < 40; i++)
+            {
+                Assert.True(coefficients.Polynomial(i).IsPrime());
+            }
+
+            Assert.False(coefficients.Polynomial(40).IsPrime());
+        }
+
+        [Fact]
+        public void PolynomialTest_EulerProjectProblem27Example2_int()
+        {
+            var coefficients = new int[] { 1, -79, 1601 };
+            for (int i = 0; i < 80; i++)
+            {
+                Assert.True(coefficients.Polynomial(i).IsPrime());
+            }
+
+            Assert.False(coefficients.Polynomial(80).IsPrime());
+        }
+
+        // long
+
+        [Fact]
+        public void PolynomialTestingNullArray_long()
+        {
+           long[] p = null;
+
+            Assert.Equal(0L, p.Polynomial(0L));
+            Assert.Equal(0L, p.Polynomial(100L));
+            Assert.Equal(0L, p.Polynomial(-50L));
+        }
+
+        [Fact]
+        public void PolynomialTestingEmptyArray_long()
+        {
+            var p = new long[] { };
+
+            Assert.Equal(0L, p.Polynomial(0L));
+            Assert.Equal(0L, p.Polynomial(100L));
+            Assert.Equal(0L, p.Polynomial(-50L));
+        }
+
+        [Theory]
+        [InlineData(0L, 5L)]
+        [InlineData(-1L, 5L)]
+        [InlineData(1L, 5L)]
+        public void PolynomialTestingOneTerm_long(long x, long c)
+        {
+            var p = new long[] { c };
+
+            Assert.Equal(c, p.Polynomial(x));
+        }
+
+        [Theory]
+
+        [InlineData(1, 0, new long[] { 1L, 1L })]
+        [InlineData(2, 1, new long[] { 1L, 1L })]
+        [InlineData(5, 4, new long[] { 1L, 1L })]
+
+        [InlineData(3, 1, new long[] { 5L, 2L, -4L, 0L })]
+        [InlineData(655, 5, new long[] { 5L, 2L, -4L, 0L })]
+
+        [InlineData(0, 1, new long[] { 5L, 2L, -4L, -3L })]
+        [InlineData(660, 5, new long[] { 5L, 2L, -4L, 5L })]
+        public void PolynomialTest_long(long expected, long x, long[] coefficients)
+        {
+            Assert.Equal(expected, coefficients.Polynomial(x));
+        }
+
+        [Fact]
+        public void PolynomialTest_EulerProjectProblem27Example1_long()
+        {
+            var coefficients = new long[] { 1L, 1L, 41L };
+            for (long i = 0; i < 40L; i++)
+            {
+                Assert.True(coefficients.Polynomial(i).IsPrime());
+            }
+
+            Assert.False(coefficients.Polynomial(40L).IsPrime());
+        }
+
+        [Fact]
+        public void PolynomialTest_EulerProjectProblem27Example2_long()
+        {
+            var coefficients = new long[] { 1L, -79L, 1601L };
+            for (long i = 0; i < 80L; i++)
+            {
+                Assert.True(coefficients.Polynomial(i).IsPrime());
+            }
+
+            Assert.False(coefficients.Polynomial(80L).IsPrime());
         }
 
     }
--- NumberUtil/PolynomialX.cs
+++ NumberUtil/PolynomialX.cs
@@ -2,29 +2,64 @@
 
 namespace NumberUtil
 {
+    /// <summary>
+    /// Extensions methods of use when working with polynomials.
+    /// </summary>
     public static class PolynomialX
     {
+        /// <summary>
+        /// Evaluates.
+        /// </summary>
+        /// <param name="coefficients">
+        /// Coefficients of the polynomial: Coefficient of first term is coefficients[n-1] where n is length of the coefficients array and coefficient of last term is coefficients[0].
+        /// </param>
+        /// <param name="x">Value to evaluate at</param>
+        /// <returns>Evaluated value</returns>
         public static int Polynomial(this int[] coefficients, int x)
         {
             var n = (coefficients == null) ? 0 : coefficients.Length;
-            
             if (n == 0) return 0;
 
-            var retVal = 0;
-            for (int i = n - 1, exp = 0; i >= 0; i--, exp++)
+            var nm1 = n - 1;
+            var retVal = coefficients[nm1];
+            if (nm1 == 0) return retVal;
+
+            var nm2 = n - 2;
+            retVal += coefficients[nm2] * x;
+            if (nm2 == 0) return retVal;
+            
+            for (int i = n - 3, exp = 2; i >= 0; i--, exp++)
             {
-                if (i == n - 1)
-                {
-                    retVal += coefficients[i];
-                }
-                else if (i == n - 2)
-                {
-                    retVal += coefficients[i] * x;
-                }
-                else
-                {
-                    retVal += coefficients[i] * (int)Math.Pow(x, exp);
-                }
+                retVal += coefficients[i] * (int)Math.Pow(x, exp);   
+            }
+
+            return retVal;
+        }
+
+        /// <summary>
+        /// Evaluates.
+        /// </summary>
+        /// <param name="coefficients">
+        /// Coefficients of the polynomial: Coefficient of first term is coefficients[n-1] where n is length of the coefficients array and coefficient of last term is coefficients[0].
+        /// </param>
+        /// <param name="x">Value to evaluate at</param>
+        /// <returns>Evaluated value</returns>
+        public static long Polynomial(this long[] coefficients, long x)
+        {
+            var n = (coefficients == null) ? 0 : coefficients.Length;
+            if (n == 0) return 0;
+
+            var nm1 = n - 1;
+            var retVal = coefficients[nm1];
+            if (nm1 == 0) return retVal;
+
+            var nm2 = n - 2;
+            retVal += coefficients[nm2] * x;
+            if (nm2 == 0) return retVal;
+
+            for (int i = n - 3, exp = 2; i >= 0; i--, exp++)
+            {
+                retVal += coefficients[i] * (long)Math.Pow(x, exp);
             }
 
             return retVal;
