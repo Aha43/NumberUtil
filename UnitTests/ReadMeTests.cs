b3b234e861ab684117224376ec21178780d961e0
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-01-04 00:26:02 +0100

    #30

---
+++ UnitTests/ReadMeTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NumberUtil
+{
+    public class ReadMeTests
+    {
+        [Fact]
+        public void T1()
+        {
+            Assert.True(101.IsPrime()); // return true
+            Assert.False(99.IsPrime()); // return false
+        }
+
+    }
+
+}
