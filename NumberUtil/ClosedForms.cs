f85dd7f2cab119e3aeff7c49dfdb2b5a0fcfb41f
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-01-25 17:24:46 +0100

    #50

--- NumberUtil/ClosedForms.cs
+++ NumberUtil/ClosedForms.cs
@@ -1,37 +1,48 @@
-﻿namespace NumberUtil
+﻿using System;
+
+using static NumberUtil.Constants;
+
+namespace NumberUtil
 {
     /// <summary>
     /// Sum of number sequences given by their closed form formula.
     /// </summary>
-    public static class ClosedFormsX
+    public static class ClosedForms
     {
         /// <summary>
         /// Sum of sequence of natural numbers: Sum(0, 1, 2, 3, 4... n).
         /// </summary>
         /// <param name="n">Number of terms</param>
         /// <returns>Sum</returns>
-        public static int SumOfNumbers(this int n) => n * (n + 1) / 2;
+        public static int SumOfNumbers(int n) => n * (n + 1) / 2;
 
         /// <summary>
         /// Sum of sequence of natural numbers: Sum(0, 1, 2, 3, 4... n).
         /// </summary>
         /// <param name="n">Number of terms</param>
         /// <returns>Sum</returns>
-        public static long SumOfNumbers(this long n) => n * (n + 1) / 2;
+        public static long SumOfNumbers(long n) => n * (n + 1) / 2;
 
         /// <summary>
         /// Sum of sequence of natural numbers squared: Sum(0, 1, 4, 9, 16, 25... n).
         /// </summary>
         /// <param name="n">Number of terms</param>
         /// <returns>Sum</returns>
-        public static int SumOfSquares(this int n) => n * (n + 1) * (2 * n + 1) / 6;
+        public static long SumOfSquares(int n) => n * (n + 1) * (2 * n + 1) / 6;
 
         /// <summary>
         /// Sum of sequence of natural numbers squared: Sum(0, 1, 4, 9, 16, 25... n).
         /// </summary>
         /// <param name="n">Number of terms</param>
         /// <returns>Sum</returns>
-        public static long SumOfSquares(this long n) => n * (n + 1) * (2 * n + 1) / 6;
+        public static long SumOfSquares(long n) => n * (n + 1) * (2 * n + 1) / 6;
+
+        /// <summary>
+        /// Sum of the Fibonacci sequence.
+        /// </summary>
+        /// <param name="n">Number of terms</param>
+        /// <returns>Sum</returns>
+        public static long SumOfFibonaccis(long n) => (long)Math.Floor((Math.Pow(GoldenRatio, n) / SquareRootOf5) + 0.5);
 
     }
 
--- UnitTests/ClosedFormsXTests.cs
+++ UnitTests/ClosedFormsXTests.cs
@@ -1,16 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using Xunit;
+using static NumberUtil.ClosedForms;
 
 namespace NumberUtil
 {
     public class ClosedFormsXTests
     {
-        public void SumOfNumbers()
+        [Theory]
+        [InlineData(5)]
+        public void SumOfNumbers(int n) => SumTester(Sequences.Natural, n, ClosedForms.SumOfNumbers);
+
+        [Theory]
+        [InlineData(5)]
+        public void SquareOfNumbers(int n) => SumTester(Sequences.Squares, n, ClosedForms.SumOfSquares);
+
+        //[Theory]
+        //[InlineData(1)]
+        //[InlineData(2)]
+        //[InlineData(3)]
+        //[InlineData(5)]
+        //public void Fibonacci(int n) => SumTester(Sequences.Fibonacci, n, ClosedFormsX.SumOfFibonaccis);
+
+        [Fact]
+        public void Fibonacci3()
         {
-            // TODO
-            //Assert.Equal(0.SumO)
+            var computed = SumOfFibonaccis(3);
+            Assert.Equal(2, computed);
         }
+
+        [Fact]
+        public void Fibonacci4()
+        {
+            var computed = SumOfFibonaccis(4);
+            Assert.Equal(4, computed);
+        }
+
+        [Fact]
+        public void Fibonacci5()
+        {
+            var computed = SumOfFibonaccis(5);
+            Assert.Equal(7, computed);
+        }
+
+        private static void SumTester(IEnumerable<long> sequences, int n, Func<long, long> closedForm)
+        {
+            var elements = sequences.Take(n).ToArray();
+            var expected = 0L;
+            for (var i = 0L; i < n; i++)
+            {
+                expected += elements[i];
+                var computed = closedForm(i);
+                Assert.Equal(expected, computed);
+            }
+        }
+
     }
+
 }
