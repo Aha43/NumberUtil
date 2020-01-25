97c5bd69a88bb42a50ebc3a276078d933098f6f2
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-01-25 17:23:27 +0100

    Added static class for constants

---
+++ NumberUtil/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumberUtil
+{
+    /// <summary>
+    /// Constants (do not duplicate constants found in Math).
+    /// </summary>
+    public static class Constants
+    {
+        public static double SquareRootOf2 { get; } = Math.Sqrt(2);
+        public static double SquareRootOf5 { get; } = Math.Sqrt(5);
+        public static double GoldenRatio { get; } = (1 + SquareRootOf5) * 0.5;
+
+    }
+}
