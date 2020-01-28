132bdc3343aec479d63694ea4867fa4d1e747091
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-01-28 21:38:15 +0100

    #18

--- NumberUtil/IEnumerableX.cs
+++ NumberUtil/IEnumerableX.cs
@@ -7,17 +7,37 @@ namespace NumberUtil
     /// </summary>
     public static class IEnumerableX
     {
+        /// <summary>
+        /// Computes the product of numbers.
+        /// </summary>
+        /// <param name="sequence">Numbers</param>
+        /// <returns>Product</returns>
         public static int Product(this IEnumerable<int> sequence)
         {
             var retVal = 1;
-            foreach (var f in sequence) retVal *= f;
+            foreach (var f in sequence)
+            {
+                if (f == 0) return 0;
+                retVal *= f;
+            }
+            
             return retVal;
         }
 
+        /// <summary>
+        /// Computes the product of numbers.
+        /// </summary>
+        /// <param name="sequence">Numbers</param>
+        /// <returns>Product</returns>
         public static long Product(this IEnumerable<long> sequence)
         {
             var retVal = 1L;
-            foreach (var f in sequence) retVal *= f;
+            foreach (var f in sequence)
+            {
+                if (f == 0) return 0L;
+                retVal *= f;
+            }
+
             return retVal;
         }
 
---
+++ UnitTests/IEnumerableXTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NumberUtil
+{
+    public class IEnumerableXTests
+    {
+        [Theory]
+        [InlineData(0, new int[] { 0 })]
+        [InlineData(0, new int[] { 1, 0 })]
+        [InlineData(0, new int[] { 0, 0 })]
+        [InlineData(0, new int[] { 0, 1 })]
+        [InlineData(0, new int[] { 9, 8, 7, 0, 10, 0, 4 })]
+
+        [InlineData(120, new int[] { 1, 2, 3, 4, 5 })]
+        [InlineData(120, new int[] { 2, 3, 4, 5 })]
+        [InlineData(-120, new int[] { 2, 3, -4, 5 })]
+        public void ProductShouldBeInt(int expected, int[] f)
+        {
+            var p = f.Product();
+            Assert.Equal(expected, p);
+        }
+
+        [Theory]
+        [InlineData(0, new long[] { 0 })]
+        [InlineData(0, new long[] { 1, 0 })]
+        [InlineData(0, new long[] { 0, 0 })]
+        [InlineData(0, new long[] { 0, 1 })]
+        [InlineData(0, new long[] { 9, 8, 7, 0, 10, 0, 4 })]
+
+        [InlineData(120, new long[] { 1, 2, 3, 4, 5 })]
+        [InlineData(120, new long[] { 2, 3, 4, 5 })]
+        [InlineData(-120, new long[] { 2, 3, -4, 5 })]
+        public void ProductShouldBeLong(long expected, long[] f)
+        {
+            var p = f.Product();
+            Assert.Equal(expected, p);
+        }
+
+
+
+    }
+
+}
